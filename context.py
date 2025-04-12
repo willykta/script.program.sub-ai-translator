ae861050c76665c6c88f6df44839799ffd3c63f5
Re <dariusz23@gmail.com>
2025-04-12 13:36:01 +0200

    added context menu for movies

---
+++ context.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import glob
+import xbmc
+import xbmcgui
+import xbmcaddon
+
+addon = xbmcaddon.Addon()
+addon_path = addon.getAddonInfo('path')
+_ = addon.getLocalizedString
+
+def resolve_path():
+    if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
+        return sys.argv[1]
+    
+    # Fallback: próbujemy wyciągnąć ścieżkę z ListItem.*
+    path = xbmc.getInfoLabel("ListItem.Path")
+    filename = xbmc.getInfoLabel("ListItem.FilenameAndPath") or xbmc.getInfoLabel("ListItem.Filename")
+    
+    if path and filename:
+        full_path = os.path.join(path, filename) if not filename.startswith(path) else filename
+        if os.path.isfile(full_path):
+            return full_path
+
+    return None
+
+selected_path = resolve_path()
+
+if not selected_path:
+    xbmcgui.Dialog().notification(_(30006), _(30008), xbmcgui.NOTIFICATION_ERROR, 3000)
+    sys.exit(1)
+
+def run_translation(srt_path):
+    xbmc.executebuiltin(f'RunScript("{os.path.join(addon_path, "service.py")}", "{srt_path}")')
+
+if selected_path.lower().endswith(".srt"):
+    run_translation(selected_path)
+else:
+    folder = os.path.dirname(selected_path)
+    srt_files = sorted(glob.glob(os.path.join(folder, "*.srt")))
+    if not srt_files:
+        xbmcgui.Dialog().notification(_(30006), _(30009), xbmcgui.NOTIFICATION_INFO, 3000)
+        sys.exit(0)
+
+    labels = [os.path.basename(p) for p in srt_files]
+    choice = xbmcgui.Dialog().select(_(30010), labels)
+    if choice >= 0:
+        run_translation(srt_files[choice])
